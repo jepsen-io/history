283a2480a0363a9dcfba3b96da27efd3dd40e908
Kyle Kingsbury <aphyr@aphyr.com>
2024-10-18 18:33:34 -0500

    Dep upgrades
    Cheshire 5.13.0
    slf4j-simple 2.0.16
    clojure 1.12.0
    tools.logging 1.3.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [; Logging
-                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/tools.logging "1.3.0"]
                  ; Control flow, including fast single-threaded reductions
                  [dom-top "1.0.9"]
                  ; Fast reductions
@@ -17,10 +17,10 @@
                  [slingshot "0.12.2"]]
   :profiles {:dev {:dependencies
                    [[com.gfredericks/test.chuck "0.2.14"]
-                    [org.clojure/clojure "1.11.1"]
+                    [org.clojure/clojure "1.12.0"]
                     [org.clojure/test.check "1.1.1"]
-                    [org.slf4j/slf4j-simple "2.0.9"]
-                    [cheshire "5.12.0"]
+                    [org.slf4j/slf4j-simple "2.0.16"]
+                    [cheshire "5.13.0"]
                     ]}}
   ; We need to write some Java code later that depends on these interfaces &
   ; defrecords.
